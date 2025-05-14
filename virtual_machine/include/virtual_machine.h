4e04f49a1b151724a1d9cfee39941961a6cd6a48
Sam Nussio <snussio@keyschool.org>
2025-05-14 12:51:22 -0400

    Created VirtualMachine struct and funcs

--- virtual_machine/include/virtual_machine.h
+++ virtual_machine/include/virtual_machine.h
@@ -1,1 +1,26 @@
-;
+#ifndef VIRTUAL_MACHINE_H
+#include VIRTUAL_MACHINE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "bytecode.h"
+
+#define STACK_SIZE = 8192;
+#define MAX_CALL_DEPTH = 1024; 
+//This is roughly equivalent to cpython's recursion depth limit
+
+typedef struct {
+  int32_t stack[STACK_SIZE];
+  int32_t stack_point;
+
+  Instruction* instructions;
+  int instruction_count;
+  int instruct_point;
+} VirtualMachine;
+
+VirtualMachine vm_create(Instruction* program, int instruction_count);
+void vm_collect(VirtualMachine* vm);
+void vm_run(VirtualMachine* vm);
+void debug_print(VirtualMachine* vm); //prints straight bytecode
+
+Instruction* load_vmc(const char* filename, int* out_count);

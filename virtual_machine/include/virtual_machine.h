0847421098b7570092b0a9a22b3d9b5c0a65f746
Sam Nussio <snussio@keyschool.org>
2025-05-14 13:38:39 -0400

    Preprocessor macro debugging

--- virtual_machine/include/virtual_machine.h
+++ virtual_machine/include/virtual_machine.h
@@ -1,21 +1,21 @@
 #ifndef VIRTUAL_MACHINE_H
-#include VIRTUAL_MACHINE_H
+#define VIRTUAL_MACHINE_H
 
 #include <stdint.h>
 #include <stdbool.h>
 #include "bytecode.h"
 
-#define STACK_SIZE = 8192;
-#define MAX_CALL_DEPTH = 1024; 
+#define STACK_SIZE 8192
+#define MAX_CALL_DEPTH 1024
 //This is roughly equivalent to cpython's recursion depth limit
 
 typedef struct {
   int32_t stack[STACK_SIZE];
-  int32_t stack_point;
+  int32_t stack_pointer;
 
   Instruction* instructions;
   int instruction_count;
-  int instruct_point;
+  int instruct_pointer;
 } VirtualMachine;
 
 VirtualMachine vm_create(Instruction* program, int instruction_count);

42e3a5535bfa69fbe7af01e222f46faf18607f9e
Sam Nussio <snussio@keyschool.org>
2025-05-14 11:21:02 -0400

    Add opcode enum and struct

--- virtual_machine/include/bytecode.h
+++ virtual_machine/include/bytecode.h
@@ -1,1 +1,26 @@
+#ifndef BYTECODE_H
+#include BYTECODE_H
 
+typedef enum {
+  OP_NOP,
+  OP_PUSH_INT,
+  OP_PUSH_STR,
+  OP_ADD,
+  OP_SUB,
+  OP_MUL,
+  OP_DIV,
+  OP_PRINT,
+  OP_STORE_VAR,
+  OP_LOAD_VAR,
+  OP_JMP,
+  OP_CALL,
+  OP_RET,
+  OP_STOP
+} OpCode;
+
+typedef struct{
+  OpCode opcode;
+  int operand;
+}Instruction;
+
+#endif

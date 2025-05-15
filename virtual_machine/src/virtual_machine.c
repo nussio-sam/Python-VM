d0d9c3a0fff48abe29773e83c7ac0ed27e9e00c2
Sam Nussio <snussio@keyschool.org>
2025-05-15 10:16:35 -0400

    Create vm_create and vm_collect

--- virtual_machine/src/virtual_machine.c
+++ virtual_machine/src/virtual_machine.c
@@ -1,1 +1,20 @@
-;
+#include <stdio.h>
+#include <stdlib.h>
+#include "virtual_machine.h"
+#include "bytecode.h"
+
+VirtualMachine* vm_create(Instruction* program, int instruction_count) {
+  VirtualMachine vm = malloc[sizeof(VirtualMachine)]
+  if(!vm) {
+    fprintf(stderr, 'Failed to allocate memory');
+    exit(1);
+  }
+  vm->instructions = program;
+  vm->instruction_count = instruction_count;
+  vm->instruct_pointer = 0;
+  vm->stack_pointer = 0;
+  
+}
+void vm_collect(VirtualMachine* vm) {
+  free(vm);
+}

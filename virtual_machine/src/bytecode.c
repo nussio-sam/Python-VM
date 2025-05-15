c2d61d904f8ceab97962de90a8d8bdca9f6c6ef1
Sam Nussio <snussio@keyschool.org>
2025-05-15 11:01:17 -0400

    create VMC compatibility and error handling

--- virtual_machine/src/bytecode.c
+++ virtual_machine/src/bytecode.c
@@ -1,1 +1,52 @@
-;
+#include <stdio.h>
+#include <stdlib.h>
+#include "bytecode.h"
+
+const char* OP_NAMES[] = {
+  "NOP",
+  "PUSH_INT",
+  "PUSH_STR",
+  "ADD",
+  "SUB",
+  "MUL",
+  "DIV",
+  "PRINT",
+  "STORE_VAR",
+  "LOAD_VAR",
+  "JMP",
+  "CALL",
+  "RET",
+  "STOP"
+};
+
+void print_instruct(Instruction instruct, int index) {
+  const char* name = "UNKNOWN";
+  if (instruct.opcode >= 0 && instruct.opcode <= OP_STOP) {
+    name = OP_NAMES[instr.opcode];
+  }
+  printf("%04d: %-14s %d\n", index, name, instr.operand);
+}
+
+void disassemble(Instruction* program, int count) {
+  for(int i = 0; i < count; i++) {
+    print_instruct(program[i], i);
+  }
+}
+
+Instruction* load_vmc(const char* filename, int* out_count) {
+  FILE* vmc = fopen(filename, 'rb')
+  if(!vmc) {
+    fprintf(stderr, "Could not open VMC file %s\n", filename);
+    return NULL;
+  }
+
+  int count;
+  fread(&count, sizeof(int), 1, vmc);
+
+  Instruction* program = malloc(sizeof(Instruction) * count);
+  fread(program, sizeof(Instruction), count, vmc);
+  fclose(vmc);
+
+  *out_count = count;
+  return program;
+}

e2b3a882771339c82815c4a80de4f378ed841d10
Sam Nussio <snussio@keyschool.org>
2025-05-15 11:11:01 -0400

    Error handling for invalid input params

--- virtual_machine/src/virtual_machine.c
+++ virtual_machine/src/virtual_machine.c
@@ -4,17 +4,29 @@
 #include "bytecode.h"
 
 VirtualMachine* vm_create(Instruction* program, int instruction_count) {
-  VirtualMachine vm = malloc[sizeof(VirtualMachine)]
+  if (!program || instruction_count <= 0) {
+    fprintf(stderr, "Invalid input parameters");
+    exit(1);
+  }
+  
+  VirtualMachine vm = malloc(sizeof(VirtualMachine));
   if(!vm) {
-    fprintf(stderr, 'Failed to allocate memory');
+    fprintf(stderr, "Failed to allocate memory");
     exit(1);
   }
   vm->instructions = program;
   vm->instruction_count = instruction_count;
   vm->instruct_pointer = 0;
   vm->stack_pointer = 0;
+
+  return vm
   
 }
 void vm_collect(VirtualMachine* vm) {
-  free(vm);
+  if(vm) {
+    free(vm);
+  } else {
+    fprintf(stderr, "No VM running!");
+    return NULL;
+  }
 }

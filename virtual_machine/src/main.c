0c0a4b82e1f2e0203a0c376128e66558fe1c33a2
Sam Nussio <snussio@keyschool.org>
2025-05-20 18:14:16 -0400

    Create main VM loop

--- virtual_machine/src/main.c
+++ virtual_machine/src/main.c
@@ -1,1 +1,20 @@
-;
+#include <stdio.h>
+#include "virtual_machine.h"
+#include "bytecode.h"
+
+int main(int argc, char *argv[]) {
+  VirtualMachine* vm = NULL;
+  VMStatus status;
+
+  //deal with creating program from python code HERE
+  program = NULL;
+  
+  status = vm_create(&vm, program, sizeof(program) / sizeof(program[0]));
+  if (status != VM_OK) {
+    fprintf(stderr, "creating VM failure: %s\n", status);
+    return 1;
+  }
+
+  vm_collect(vm);
+  return 0;
+}

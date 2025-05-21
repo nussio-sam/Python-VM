5afe68832ec9dba49a7016572967953c487d651c
Sam Nussio <snussio@keyschool.org>
2025-05-21 02:19:43 -0400

    Simplify scope of main.c

--- virtual_machine/src/main.c
+++ virtual_machine/src/main.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
 #include "virtual_machine.h"
-#include "bytecode.h"
 
 int main(int argc, char *argv[]) {
-  VirtualMachine* vm = NULL;
-  VMStatus status;
+    //deal with creating program from python code HERE
+    Instruction program = { //example program for clarity
+        OP_PUSH_INT,
+        10,
+    };
 
-  //deal with creating program from python code HERE
-  program = NULL;
-  
-  status = vm_create(&vm, program, sizeof(program) / sizeof(program[0]));
-  if (status != VM_OK) {
-    fprintf(stderr, "creating VM failure: %s\n", status);
-    return 1;
-  }
+    VirtualMachine vm = vm_create(&program, sizeof(program));
 
-  vm_collect(vm);
-  return 0;
+    vm_collect(&vm);
+    return 0;
 }

5f5ff9fe3491707df5c6bdd91a4c7ee8a3f27d88
Sam Nussio <nussiosam@gmail.com>
2025-08-04 22:16:23 -0400

    Fixing fatal syntax errors

--- virtual_machine/src/bytecode.c
+++ virtual_machine/src/bytecode.c
@@ -22,7 +22,7 @@ const char* OP_NAMES[] = {
 void print_instruct(Instruction instruct, int index) {
   const char* name = "UNKNOWN";
   if (instruct.opcode >= 0 && instruct.opcode <= OP_STOP) {
-    name = OP_NAMES[instr.opcode];
+    name = OP_NAMES[instruct.opcode];
   }
   printf("%04d: %-14s %d\n", index, name, instr.operand);
 }
@@ -34,19 +34,33 @@ void disassemble(Instruction* program, int count) {
 }
 
 Instruction* load_vmc(const char* filename, int* out_count) {
-  FILE* vmc = fopen(filename, 'rb')
+  FILE* vmc = fopen(filename, 'rb');
   if(!vmc) {
     fprintf(stderr, "Could not open VMC file %s\n", filename);
     return NULL;
   }
 
   int count;
-  fread(&count, sizeof(int), 1, vmc);
-
-  Instruction* program = malloc(sizeof(Instruction) * count);
-  fread(program, sizeof(Instruction), count, vmc);
-  fclose(vmc);
+  if (fread(&count, sizeof(int), 1, vmc) != 1) {
+    fprintf(stderr, "Failed to read instruction count from %s\n", filename);
+    fclose(vmc);
+    return NULL;
+  }
+  Instruction* program = calloc(count, sizeof(Instruction));
+  if (!program) {
+    fprintf(stderr, "Out of Memory\n");
+    fclose(vmc);
+    return NULL;
+  }
+  
+  if (fread(program, sizeof(Instruction), count, vmc) != count) {
+    fprintf(stderr, "Failed to read instruction data from %s\n", filename);
+    free(program);
+    fclose(vmc);
+    return NULL;
+}
 
   *out_count = count;
+  fclose(vmc)
   return program;
 }
